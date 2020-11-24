645a0b082257160b6ad8d308163b28bc934872c9
NikolasKorakidis <korakidisn@gmail.com>
2020-11-24 23:18:49 +0100

    step 2

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,40 @@
-console.log("test");
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("viewing dashboard") },
+    { path: "/posts", view: () => console.log("viewing posts") },
+    { path: "/setting", view: () => console.log("viewing settings") },
+  ];
+
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find((potencialMatch) => potencialMatch.isMatch);
+
+  if (!match) {
+    match = {
+      route: route[0],
+      isMatch: true,
+    };
+  }
+
+  console.log(match);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
